072328c19abb85a35c515be6fc1eae4f64c08fe5
GucciPotato1 <87156516+GucciPotato1@users.noreply.github.com>
2021-09-16 18:08:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var list_of_guests=[];
+function submit(){
+       var GuestName = document.getElementById("namess").value;
+    list_of_guests.push(GuestName);
+    document.getElementById("list_of_guests_display").innerHTML=list_of_guests;
+    
+} 
+
+function show(){
+    var x=list_of_guests.join('<br>');
+    document.getElementById("listnumber2").innerHTML=x;
+}
+
+function sorting(){
+    list_of_guests.sort();
+    var x=list_of_guests.join('<br>');
+    document.getElementById
+}
+
+function searching(){
+    var y= document.getElementById("").value;
+    for(var i=0; i<list_of_guests.length; i++){
+        document.getElementById("").innerHTML=list_of_guests[i];
+    }
+}
